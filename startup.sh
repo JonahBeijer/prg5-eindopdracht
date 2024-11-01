3c141415e0e19bdfccb8e9acc259b6ab2d26cb6d
jonah <jonahbeijer@gmail.com>
2024-11-01 18:06:25 +0100

    puntjes op de i

--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
-# Start Laravel server on port 8080
-php /home/site/wwwroot/artisan serve --host=0.0.0.0 --port=8080
+# Start Laravel server op poort 8080
+php /home/site/wwwroot/artisan serve --host=0.0.0.0 --port=8181
